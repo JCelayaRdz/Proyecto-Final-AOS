800c053058336abb527c41acc86d7623346f3571
JCelayaRdz <jcelayardz@gmail.com>
2023-05-08 16:34:07 +0200

    se crea el proyecto

---
+++ src/main/java/org/grupo6aos/apigestionclientes/ApiGestionClientesApplication.java
@@ -0,0 +1,13 @@
+package org.grupo6aos.apigestionclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/grupo6aos/apigestionclientes/ApiGestionClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grupo6aos.apigestionclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
