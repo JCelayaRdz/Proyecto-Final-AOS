c957280f901a7ac141b5eb35964283894398f38e
JCelayaRdz <jcelayardz@gmail.com>
2023-05-09 12:01:57 +0200

    se añade paginacion al repositorio

--- src/main/java/org/grupo6aos/apigestionclientes/repository/ClienteRepository.java
+++ src/main/java/org/grupo6aos/apigestionclientes/repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package org.grupo6aos.apigestionclientes.repository;
 
 import org.grupo6aos.apigestionclientes.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
+public interface ClienteRepository extends JpaRepository<Cliente, String>,
+        PagingAndSortingRepository<Cliente, String> {
 }
