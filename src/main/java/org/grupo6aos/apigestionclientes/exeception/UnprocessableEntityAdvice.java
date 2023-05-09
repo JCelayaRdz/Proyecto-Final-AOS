5f99d54aaa5fa131a630fd876fea7babcf38cd75
JCelayaRdz <jcelayardz@gmail.com>
2023-05-09 17:44:44 +0200

    se añade advice para exepcecion de validacion request body

---
+++ src/main/java/org/grupo6aos/apigestionclientes/exeception/UnprocessableEntityAdvice.java
@@ -0,0 +1,27 @@
+package org.grupo6aos.apigestionclientes.exeception;
+
+import org.grupo6aos.apigestionclientes.model.HttpProblem;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class UnprocessableEntityAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<HttpProblem> unprocessableEntityHandler() {
+        var body = new HttpProblem(
+                "https://httpstatuses.com/422",
+                "UNPROCESSABLE ENTITY",
+                422,
+                "Faltan atributos obligatorios",
+                "about:blank"
+        );
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .header("Content-Type","application/problem+json")
+                .body(body);
+    }
+}
