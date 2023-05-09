a8c55e1ada065a8f6348adcec061d719574b66e3
JCelayaRdz <jcelayardz@gmail.com>
2023-05-09 17:06:54 +0200

    se modifica endpoint para que devuelva un ETag

--- src/main/java/org/grupo6aos/apigestionclientes/controller/ClienteController.java
+++ src/main/java/org/grupo6aos/apigestionclientes/controller/ClienteController.java
@@ -1,11 +1,14 @@
 package org.grupo6aos.apigestionclientes.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.grupo6aos.apigestionclientes.model.Cliente;
 import org.grupo6aos.apigestionclientes.service.ClienteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,19 +19,26 @@ public class ClienteController {
 
     private final ClienteService service;
 
+    private final Logger log = LoggerFactory.getLogger(ClienteController.class);
+
     public ClienteController(ClienteService service) {
         this.service = service;
     }
 
     @GetMapping
-    public List<Cliente> findAll(
+    public ResponseEntity<List<Cliente>> findAll(
             @RequestParam(name = "page", required = false) Integer page,
             @RequestParam(name = "order", required = false) String order,
-            @RequestParam(name = "ordering", required = false) Sort.Direction ordering
+            @RequestParam(name = "ordering", required = false) Sort.Direction ordering,
+            HttpServletRequest request
     ) {
-        return service.findAll(page, order, ordering)
+        var clientes =  service.findAll(page, order, ordering)
                 .stream()
                 .toList();
+        String etag = Integer.toString(clientes.hashCode());
+        return ResponseEntity.ok()
+                .header("ETag", etag)
+                .body(clientes);
     }
 
     @GetMapping("/{clienteId}")
