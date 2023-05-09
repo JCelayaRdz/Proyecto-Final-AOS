388b721c07ec303d9513f44e4f6a909c65111663
JCelayaRdz <jcelayardz@gmail.com>
2023-05-09 12:04:18 +0200

    se añade endpoint

--- src/main/java/org/grupo6aos/apigestionclientes/controller/ClienteController.java
+++ src/main/java/org/grupo6aos/apigestionclientes/controller/ClienteController.java
@@ -1,11 +1,10 @@
 package org.grupo6aos.apigestionclientes.controller;
 
 import org.grupo6aos.apigestionclientes.model.Cliente;
-import org.grupo6aos.apigestionclientes.repository.ClienteRepository;
 import org.grupo6aos.apigestionclientes.service.ClienteService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +19,16 @@ public class ClienteController {
     }
 
     @GetMapping
-    public List<Cliente> findAll() {
-        return service.findAll();
+    public Slice<Cliente> findAll(
+            @RequestParam(name = "page", required = false) Integer page,
+            @RequestParam(name = "order", required = false) String order,
+            @RequestParam(name = "ordering", required = false) Sort.Direction ordering
+    ) {
+        return service.findAll(page, order, ordering);
+    }
+
+    @GetMapping("/{clienteId}")
+    public Cliente findOne(@PathVariable String clienteId) {
+        return service.findOne(clienteId);
     }
 }
--- src/main/java/org/grupo6aos/apigestionclientes/service/ClienteService.java
+++ src/main/java/org/grupo6aos/apigestionclientes/service/ClienteService.java
@@ -3,9 +3,10 @@ package org.grupo6aos.apigestionclientes.service;
 import org.grupo6aos.apigestionclientes.exeception.NotFoundException;
 import org.grupo6aos.apigestionclientes.model.Cliente;
 import org.grupo6aos.apigestionclientes.repository.ClienteRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ClienteService {
@@ -16,11 +17,15 @@ public class ClienteService {
         this.repository = repository;
     }
 
-    public List<Cliente> findAll() {
-        var clientes = repository.findAll();
-        if (clientes.isEmpty()) {
-            throw new NotFoundException();
+    public Slice<Cliente> findAll(Integer page, String order, Sort.Direction ordering) {
+        if (ordering != Sort.Direction.DESC) {
+            return repository.findAll(PageRequest.of(page, 5, Sort.by(order).ascending()));
         }
-        return clientes;
+        return repository.findAll(PageRequest.of(page, 5, Sort.by(order).descending()));
+    }
+
+    public Cliente findOne(String clienteId) {
+        return repository.findById(clienteId)
+                .orElseThrow(NotFoundException::new);
     }
 }
