4a4bc14999ab4471e56853dde2a6ef06786e11b3
JCelayaRdz <jcelayardz@gmail.com>
2023-05-10 23:59:47 +0200

    se añaden datos de prueba

---
+++ src/main/resources/sample-data.sql
@@ -0,0 +1,10 @@
+INSERT IGNORE INTO cliente
+    (id, nombre, apellidos, sexo, edad, numero_telefono, correo_electronico, nombre_calle, numero_edificacion, detalles)
+    VALUES
+    ('18350856F', 'Pedro', 'Alarcon Aguado', 'MASCULINO', 23, '755671849', 'paa@gmail.com', 'C/ Sol', 12, null),
+    ('95059365C', 'Juan', 'del Expósito', 'MASCULINO', 45, '+34650707510', 'jde@gmail.com', 'C/ Luna', 12, '2A'),
+    ('X1246359N', 'Fermín', 'Malo Posada', 'MASCULINO', 42, '617857326', 'fmp@gmail.com', 'C/ Playa', 12, null),
+    ('31159045W', 'Felicia', 'Duarte Alcaraz', 'FEMENINO', 20, '+347110805173', 'fda@gmail.com', 'C/ Cuba', 12, '1R'),
+    ('00413395Q', 'Guillermo', 'Osuna', 'OTRO', 67, '672670731', 'gos@gmail.com', 'C/ Ecuador', 12, null),
+    ('Y1677125W', 'Leandro', 'Becerra-Coca', 'MASCULINO', 28, '+34651796071', 'lbc@gmail.com', 'C/ Mexico', 12, '3F'),
+    ('54805140G', 'Darío', 'Escolano Anaya', 'MASCULINO', 33, '785282323', 'dea@gmail.com', 'C/ Colombia', 12, null);
