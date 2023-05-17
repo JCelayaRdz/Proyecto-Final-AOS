c6f977859f75359d346c75535928447b11196da0
JCelayaRdz <jcelayardz@gmail.com>
2023-05-17 18:26:51 +0200

    se modifica servicio para que añada links

--- src/main/java/org/grupo6aos/apigestionclientes/service/ClienteService.java
+++ src/main/java/org/grupo6aos/apigestionclientes/service/ClienteService.java
@@ -1,26 +1,43 @@
 package org.grupo6aos.apigestionclientes.service;
 
+import org.grupo6aos.apigestionclientes.dto.ClienteDto;
+import org.grupo6aos.apigestionclientes.dto.Link;
 import org.grupo6aos.apigestionclientes.exeception.BadRequestException;
 import org.grupo6aos.apigestionclientes.exeception.NotFoundException;
 import org.grupo6aos.apigestionclientes.model.Cliente;
 import org.grupo6aos.apigestionclientes.repository.ClienteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class ClienteService {
 
     private final ClienteRepository repository;
 
+    private final VinService vinService;
+
     private final static int PAGE_SIZE = 5;
 
-    public ClienteService(ClienteRepository repository) {
+    private final static String VEHICULOS_URL = "https://example.com/api/v1/vehiculos";
+
+    private final Logger log = LoggerFactory.getLogger(ClienteService.class);
+
+    public ClienteService(ClienteRepository repository,
+                          VinService vinService) {
         this.repository = repository;
+        this.vinService = vinService;
     }
 
-    public Slice<Cliente> findAll(Integer page, String order, Sort.Direction ordering) {
+    public List<ClienteDto> findAll(Integer page,
+                                    String order,
+                                    Sort.Direction ordering,
+                                    String url) {
         int pageNumber = 0;
         String orderBy = "id";
         Sort.Direction direction = Sort.Direction.ASC;
@@ -41,7 +58,45 @@ public class ClienteService {
 
         if (slice.getNumberOfElements() == 0) throw new NotFoundException();
 
-        return slice;
+        var c =  slice.stream()
+                .map(Cliente::toDto)
+                .map(dto -> {
+                    dto.setVehiculos(vinService.generateVins());
+                    return dto;
+                })
+                .map(dto -> {
+                    dto.setLinks(addlinks(dto, url));
+                    return dto;
+                })
+                .toList();
+
+        return c;
+    }
+
+    public Map<String, Object> addlinks(ClienteDto cliente, String url) {
+        var links = new HashMap<String, Object>();
+        var vehiculos = new ArrayList<Link>();
+        links.put("parent", new Link (
+                url,
+                "cliente_cget cliente_post"
+        ));
+
+        links.put("self", new Link (
+           url+"/"+cliente.getId(),
+                "cliente_get cliente_delete cliente_put"
+        ));
+
+        cliente.getVehiculos()
+                .forEach(vin -> {
+                    vehiculos.add(new Link(
+                            VEHICULOS_URL+"/"+vin,
+                            "vehiculo_get vehiculo_delete vehiculo_put"
+                    ));
+                });
+
+        links.put("vehiculos", vehiculos);
+
+        return links;
     }
 
     private boolean shouldReturnFirstPage(Integer page) {
@@ -56,19 +111,28 @@ public class ClienteService {
         return ordering == null || ordering.isAscending();
     }
 
-    public Cliente findOne(String clienteId) {
+    public ClienteDto findOne(String clienteId) {
         return repository.findById(clienteId)
+                .map(Cliente::toDto)
+                .map(dto -> {
+                    dto.setVehiculos(vinService.generateVins());
+                    return dto;
+                })
                 .orElseThrow(NotFoundException::new);
     }
 
-    public Cliente saveOne(Cliente cliente) {
-        if (repository.existsById(cliente.getId())) throw new BadRequestException();
-        return repository.save(cliente);
+    public ClienteDto saveOne(ClienteDto clienteDto) {
+        if (repository.existsById(clienteDto.getId())) throw new BadRequestException();
+        var cliente = repository.save(clienteDto.toEntity())
+                .toDto();
+        cliente.setVehiculos(vinService.generateVins());
+        return cliente;
     }
 
-    public Cliente updateOne(String clienteId, Cliente cliente) {
+    public ClienteDto updateOne(String clienteId, ClienteDto clienteDto) {
         if (!repository.existsById(clienteId)) throw new NotFoundException();
-        return repository.save(cliente);
+        return repository.save(clienteDto.toEntity())
+                .toDto();
     }
 
     public void deleteOne(String clienteId) {
